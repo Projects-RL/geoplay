558fe9e6315bcaeec246800a6e4eea20d777b18f
Robin Lans <roblan95@icloud.com>
2022-06-02 23:48:34 +0200

    jabbadabado

--- pages/play/index.tsx
+++ pages/play/index.tsx
@@ -3,12 +3,24 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import styles from "./index.module.css";
 import naJSON from "../../geojson/NA.geo.json";
+import MyApp from "../_app";
 
 function GamePage() {
     const mapContainer = useRef<HTMLDivElement>(null);
     const map = useRef<mapboxgl.Map | null>(null);
     const [clickedCountries, setClickedCountries] = useState<String[]>([]);
+    const [bajs, setBajs] = useState(true);
+    const [countriesList, setCountriesList] = useState<String[]>([]);
     let hoveredCountryId: any = null;
+
+    function populateCountries() {
+        const countries = naJSON.features.map((country) => {
+            return country.properties.name;
+        });
+        console.log(countries);
+
+        setCountriesList(countries);
+    }
 
     useEffect(() => {
         if (map.current) return;
@@ -37,26 +49,19 @@ function GamePage() {
                     source: "countries",
                     layout: {},
                     paint: {
-                        // "fill-color": "#000",
                         "fill-color": [
                             "case",
-                            // ["boolean", ["feature-state", "clicked"]],
-                            // "#04ff86",
-                            ["boolean", ["feature-state", "hover"], false],
-                            "#fff",
-                            "#000",
+                            ["==", ["feature-state", "correct"], true], // if correct == true
+                            "#04ff86", // ...then color the polygon this color
+                            ["==", ["feature-state", "correct"], false], // if correct == false
+                            "#e51b0e", // ...then color the polygon this color
+                            ["==", ["feature-state", "hover"], true], // if the polygon is being hovered over
+                            "#fff", // ...then color the polygon white
+                            "#000", // this is the fallback value if neither of the cases above happens
                         ],
                         "fill-opacity": 0.5,
                     },
                 });
-
-                // if(clicked && answer){
-                //     color: 'green'
-                // } else if(clicked && !answer){
-                //     color: 'red'
-                // } else if(!clicked) {
-                // color: 'nothing'
-                // }
 
                 map.current?.addLayer({
                     id: "borders",
@@ -68,8 +73,6 @@ function GamePage() {
                         "line-width": 2,
                     },
                 });
-
-                map.current?.on("click", "country-fills", pickCountry);
             });
         });
         map.current?.on("mousemove", "country-fills", (e: any) => {
@@ -96,28 +99,50 @@ function GamePage() {
             }
             hoveredCountryId = null;
         });
+        map.current.on("click", "country-fills", (e: any) => {
+            if (!countriesList) return;
+            console.log("hej");
+
+            const countryID = e.features[0].id;
+            const countryName = e.features[0].properties.name;
+
+            const countryObj = {
+                countryID,
+                countryName,
+            };
+            setClickedCountries((prevValue: any) => {
+                return [...prevValue, countryObj];
+            });
+        });
+        populateCountries();
     }, []);
 
-    function pickCountry(e: any) {
-        console.log(e.features[0].properties.name);
-        const countryID = e.features[0].id;
-        const countryName = e.features[0].properties.name;
-
-        map.current?.setFeatureState(
-            { source: "countries", id: countryID },
-            {
-                clicked: true,
-            }
-        );
-
-        const countryObj = {
-            countryID,
-            countryName,
-        };
-        setClickedCountries((prevValue: any) => {
-            return [...prevValue, countryObj];
-        });
+    if (map.current) {
     }
+
+    useEffect(() => {
+        const index = clickedCountries.length;
+        const pickedCountry: any = clickedCountries[index - 1];
+        const correctCountry = countriesList[index];
+
+        if (!pickedCountry) return;
+
+        if (pickedCountry.countryName === correctCountry) {
+            map.current?.setFeatureState(
+                { source: "countries", id: pickedCountry.countryID },
+                {
+                    correct: true,
+                }
+            );
+        } else {
+            map.current?.setFeatureState(
+                { source: "countries", id: pickedCountry.countryID },
+                {
+                    correct: false,
+                }
+            );
+        }
+    }, [clickedCountries]);
 
     return <div ref={mapContainer} className={styles.container}></div>;
 }
